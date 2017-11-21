42e56fe392e481bf33b49721991f031c90ca0ce3
Adam Harris <adamgrantharris14@gmail.com>
2017-11-21 16:01:59 -0600

    Finished App.js logic

--- Src/App.js
+++ Src/App.js
@@ -1,64 +1,59 @@
 // Sorry about excessive comments... its very hard to stay sane when working with this much RegEx
 window.onload = () => {
 
-	// Triggered when state changes... aka 'They uploaded a file'
+	// Triggered on state change
 	document.getElementById('svgUpload').addEventListener('change', readFile);
 
 	let outputDisplay = document.getElementById('displayList');
 
 	// Renders selected file into str
 	function readFile() {
-		let svgFile = this.files[0];
 		let reader = new FileReader();
 		reader.addEventListener('loadend', function () {
-			splitSvgTags(reader.result);
+			parseFile(reader.result);
 		});
-		reader.readAsText(svgFile);
+		reader.readAsText(this.files[0]);
 	};
 
-	// Splits str into groups
-	function splitSvgTags(str) {
+	// Splits str into manageable groups
+	function parseFile(str) {
 
 		// Verifying str type
 		if (typeof str !== 'string') {
-			alert('Sorry... we had a uhh... error parsing? Deepest apologies m8.');
+			alert('Sorry... we had a uhh... error parsing?');
 			return;
 		}
 
-		// This RegEx sucked to make
-		let strToParsedArr = str.match(/(["'])(?:(?=(\\?))\2.)*?\1/g);
-		formatPaths(strToParsedArr);
+		// Splitting str at quotes, then removing outside quotes
+		let newParsedArr = str.match(/['"].*?['"]/g).map(item => {
+			return item.replace(/['"]/g, "");
+		});
+		formatPaths(newParsedArr);
 	};
 
 	// Verifies paths
 	function formatPaths(parsedArr) {
 
-		// Making sure svgArr is correct format
+		// Making sure svgArr is an array
 		if (parsedArr instanceof Array) {
 
-			// Checking if str contains certain characters
+			// Path checking and formatting
 			parsedArr.map(path => {
-				if (path.indexOf('M') !== -1) {
-					if (path.match(/M/g).length > 1) {
-						let temp = path.split(/(?=M)/g);
-						temp.map(item => {
-							if (item.indexOf('M') !== -1) {
-								displayPaths(item);
-							}
-						});
-					} else { displayPaths(path); }
-				} else { return; }
+				path.split(/(?=M)/g).map(item => {
+					if (item.indexOf('M') !== -1) {
+						displayPaths(item);
+					}
+				});
 			});
 		} else {
-			alert('Tbh I have no idea what went wrong here. Apologies m8...');
-			return;
+			alert('Seems like we had an error parsing, please make sure the file is the correct format and try again.');
 		}
 	}
 
 	// Displaying paths
 	function displayPaths(paths) {
 		let li = document.createElement('li');
-		let liText = document.createTextNode(`<path d='${paths}' />`);
+		let liText = document.createTextNode(`<path d="${paths}" />`);
 		li.appendChild(liText);
 		outputDisplay.appendChild(li);
 	}
